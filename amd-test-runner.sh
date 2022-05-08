2f967610cc5f5e0a23730225e42eb98ad26772ba
Chris Hill <cnh@mit.edu>
2022-05-08 12:39:48 -0400

    Add a singularity step

--- amd-test-runner.sh
+++ amd-test-runner.sh
@@ -3,3 +3,23 @@
 module use /home/software.x86_64/modulefiles; module load singularity
 /bin/rm -f matin-julia-roc4.2.sif
 singularity pull matin-julia-roc4.2.sif docker://christophernhill/matin-julia:roc4.2
+/bin/rm -fr .julia
+export JULIA_DEPOT_PATH=`pwd`/.julia
+export TEST_GROUP=quick_amd
+export RDIR=`pwd`
+cat > myscript.jl <<EOFA
+using InteractiveUtils
+versioninfo(verbose=true)
+using Pkg
+Pkg.instantiate()
+Pkg.build()
+Pkg.precompile()
+Pkg.status()
+Pkg.test()
+EOFA
+singularity run --bind `pwd`:`pwd` matin-julia-roc4.2.sif <<!
+cd ${RDIR}
+export JULIA_DEPOT_PATH=${JULIA_DEPOT_PATH}
+export TEST_GROUP=${TEST_GROUP}
+julia --project=. myscript.jl
+!
