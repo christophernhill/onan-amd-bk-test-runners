159d5d0f72a3e2f8968002000bea63ffff2ec2d3
Chris Hill <cnh@mit.edu>
2022-05-08 11:36:22 -0400

    Add fetch container from Docker

--- amd-test-runner.sh
+++ amd-test-runner.sh
@@ -1,2 +1,5 @@
 #/bin/bash -l
 /opt/rocm/bin/rocm-smi
+module use /home/software.x86_64/modulefiles; module load singularity
+/bin/rm -f matin-julia-roc4.2.sif
+singularity pull matin-julia-roc4.2.sif docker://christophernhill/matin-julia:roc4.2
